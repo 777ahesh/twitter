fd0ed9164d39b25e23a97b893f46df1909c76671
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2025-06-01 20:02:34 +0530

    updated the image upload service

--- server/src/services/twitterService.js
+++ server/src/services/twitterService.js
@@ -1,4 +1,5 @@
 const { TwitterApi } = require('twitter-api-v2');
+const axios = require('axios');
 require('dotenv').config();
 
 class TwitterService {
@@ -36,11 +37,41 @@ class TwitterService {
         }
     }
 
+    // Helper to upload image and get media_id
+    async uploadImage(twitterClient, imageData) {
+        // imageData can be a base64 string or a URL
+        let imageBuffer;
+        if (imageData.startsWith('http')) {
+            // Download image from URL
+            const response = await axios.get(imageData, { responseType: 'arraybuffer' });
+            imageBuffer = Buffer.from(response.data, 'binary');
+        } else if (imageData.startsWith('data:')) {
+            // Base64 data URI
+            const base64 = imageData.split(',')[1];
+            imageBuffer = Buffer.from(base64, 'base64');
+        } else {
+            throw new Error('Unsupported image format');
+        }
+        // Upload to Twitter
+        const mediaId = await twitterClient.v1.uploadMedia(imageBuffer, { type: 'png' }); // or 'jpg'
+        return mediaId;
+    }
+
     // Post single tweet using access token
-    async postTweet(tweetContent, accessToken) {
+    async postTweet(tweetContent, accessToken, options = {}) {
         try {
             const twitterClient = new TwitterApi(accessToken);
-            const response = await twitterClient.v2.tweet(tweetContent);
+            let mediaIds = [];
+            if (options.imageData) {
+                const mediaId = await this.uploadImage(twitterClient, options.imageData);
+                mediaIds.push(mediaId);
+            }
+            const params = { status: tweetContent };
+            if (mediaIds.length) {
+                params.media_ids = mediaIds;
+            }
+            // v1.1 endpoint for media
+            const response = await twitterClient.v1.tweet(params.status, { media_ids: mediaIds });
             return response;
         } catch (error) {
             throw new Error('Error posting tweet: ' + error.message);
@@ -48,37 +79,29 @@ class TwitterService {
     }
 
     // Post tweet thread using access token
-    async postThread(tweets, accessToken) {
+    async postThread(tweets, accessToken, options = {}) {
         try {
             const twitterClient = new TwitterApi(accessToken);
-            const responses = [];
-            let previousTweetId = null;
-
+            let inReplyToStatusId = null;
+            let responses = [];
             for (let i = 0; i < tweets.length; i++) {
-                const tweetContent = tweets[i];
-                const tweetOptions = {};
-
-                // If this is not the first tweet, make it a reply to the previous one
-                if (previousTweetId) {
-                    tweetOptions.reply = {
-                        in_reply_to_tweet_id: previousTweetId
-                    };
+                let mediaIds = [];
+                if (i === 0 && options.imageData) {
+                    const mediaId = await this.uploadImage(twitterClient, options.imageData);
+                    mediaIds.push(mediaId);
                 }
-
-                console.log(`Posting tweet ${i + 1}/${tweets.length}:`, tweetContent);
-                
-                const response = await twitterClient.v2.tweet(tweetContent, tweetOptions);
+                const params = { status: tweets[i] };
+                if (mediaIds.length) {
+                    params.media_ids = mediaIds;
+                }
+                if (inReplyToStatusId) {
+                    params.in_reply_to_status_id = inReplyToStatusId;
+                    params.auto_populate_reply_metadata = true;
+                }
+                const response = await twitterClient.v1.tweet(params.status, params);
+                inReplyToStatusId = response.id_str;
                 responses.push(response);
-                
-                // Store the tweet ID for the next iteration
-                previousTweetId = response.data.id;
-                
-                // Add a small delay between tweets to avoid rate limiting
-                if (i < tweets.length - 1) {
-                    await new Promise(resolve => setTimeout(resolve, 1000));
-                }
             }
-
             return responses;
         } catch (error) {
             throw new Error('Error posting thread: ' + error.message);
